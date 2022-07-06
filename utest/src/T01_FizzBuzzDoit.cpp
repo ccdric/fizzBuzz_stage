8c50dd6b15dc58e5debc7ae1e67722020785cc02
Cédric Castanet <cedriccastanet@free.fr>
2022-07-06 10:40:17 +0200

    ajout Origine de l'erreur dans les tests

--- utest/src/T01_FizzBuzzDoit.cpp
+++ utest/src/T01_FizzBuzzDoit.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+#define ORIGINE (" ==> " + std::string(__PRETTY_FUNCTION__) + ", ligne = " + std::to_string(__LINE__) + " : \n")
+#define TEST_CAS_SPECIAUX(NOMBRE, ATTENDU) (testCasSpeciaux(NOMBRE, ATTENDU,ORIGINE))
+
 class T01_FizzBuzzDoit : public ::testing::Test {
 
  protected:
@@ -18,7 +21,8 @@ class T01_FizzBuzzDoit : public ::testing::Test {
     virtual void TearDown() {
     }
 
-    void testCasSpeciaux(int cas, const string & attendu, const string & position=""){
+    void testCasSpeciaux(int cas, const string & attendu, const string & position){
+        SCOPED_TRACE("\n Origine  : " + position );
         int random = rand() % 100 * cas;
         EXPECT_EQ(FizzBuzz::direUnNombre(cas), attendu);
         EXPECT_EQ(FizzBuzz::direUnNombre(cas*2), attendu);
@@ -28,17 +32,17 @@ class T01_FizzBuzzDoit : public ::testing::Test {
 
 TEST_F(T01_FizzBuzzDoit, retournerFizz)
 {
-    testCasSpeciaux(FIZZ_NBRE,"Fizz");
+    TEST_CAS_SPECIAUX(FIZZ_NBRE,"Fizz");
 }
 
 TEST_F(T01_FizzBuzzDoit, retournerBuzz)
 {
-    testCasSpeciaux(BUZZ_NBRE,"Buzz");
+    TEST_CAS_SPECIAUX(BUZZ_NBRE,"Buzz");
 }
 
 TEST_F(T01_FizzBuzzDoit, retournerFizzBuzz)
 {
-    testCasSpeciaux(FIZZ_NBRE*BUZZ_NBRE,"FizzBuzz");
+    TEST_CAS_SPECIAUX(FIZZ_NBRE*BUZZ_NBRE,"FizzBuzz");
 }
 
 TEST_F(T01_FizzBuzzDoit, retournerNombre)
